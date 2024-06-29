311a9db065a6895c7843a8fec8bfee9f4f77d191
AjwadG <ajwadelgandouz@yahoo.com>
2024-06-29 23:15:50 +0200

    4. Authenticate a user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,18 +1,34 @@
+import mongodb from 'mongodb';
 import UserCollection from '../utils/users';
+import redisClient from '../utils/redis';
 
 class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
-    if (email === undefined) {
-      res.status(400).json({ error: 'Missing email' });
-    } else if (password === undefined) {
-      res.status(400).json({ error: 'Missing password' });
-    } else if (await UserCollection.getUser({ email })) {
-      res.status(400).json({ error: 'Already exists' });
-    } else {
-      const userId = await UserCollection.createUser({ email, password });
-      res.status(201).json({ id: userId, email });
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
     }
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+    const user = await UserCollection.getUser({ email });
+    if (user && user.length > 0) {
+      return res.status(400).json({ error: 'Already exists' });
+    }
+    const userId = await UserCollection.createUser({ email, password });
+    return res.status(201).json({ id: userId, email });
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+    const id = token ? await redisClient.get(`auth_${token}`) : null;
+    if (!id) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const user = await UserCollection.getUser({
+      _id: mongodb.ObjectId(id),
+    });
+    return res.status(200).json({ id, email: user[0].email });
   }
 }
 
--- utils/users.js
+++ utils/users.js
@@ -1,18 +1,19 @@
-import dbClient from './db';
 import sha1 from 'sha1';
+import dbClient from './db';
 
 class UsersCollection {
-static async createUser({ email, password }) {
+  static async createUser({ email, password }) {
     const collection = dbClient.getCollection('users');
     const newUser = { email, password: sha1(password) };
     const commandResult = await collection.insertOne(newUser);
     return commandResult.insertedId;
-}
-static async getUser(query) {
+  }
+
+  static async getUser(query) {
     const collection = dbClient.getCollection('users');
-    const user = await collection.find(query);
+    const user = await collection.find(query).toArray();
     return user;
-}
+  }
 }
 
 export default UsersCollection;
